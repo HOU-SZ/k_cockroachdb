b1a28ec49e4822b9c50f380584088e3c7bb87428
ZhengYing <zheng@comp.nus.edu.sg>
2021-10-19 10:16:53 +0800

    Refine models

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@ from models.customer import Customer
 from models.district import District
 from models.item import Item
 from models.order import Order
-from models.order_line import OrderLine
+from models.orderline import OrderLine
 from models.stock import Stock
 from models.warehouse import Warehouse
 from models.base import database
