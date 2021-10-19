46b7b4114a2740462756368c3a82684ee3283a56
Shizheng Hou [I] <shihou@tesla.com>
2021-10-20 00:11:17 +0800

    Add Transaction 2 & 6

---
+++ transactions/payment.py
@@ -0,0 +1,46 @@
+from playhouse.cockroachdb import CockroachDatabase, DatabaseProxy
+from decimal import Decimal
+from datetime import datetime
+
+from models import *
+from transactions.base import BaseTransaction
+
+
+class PaymentTransaction(BaseTransaction):
+    def __init__(self, w_id, d_id, id, payment):
+        self.__w_id = w_id
+        self.__d_id = d_id
+        self.__id = id
+        self.__payment = payment
+
+    def _execute_transaction(self):
+
+        Warehouse.update(ytd=Warehouse.ytd +
+                         self.__payment).where(Warehouse.id == self.__w_id).execute()
+        District.update(ytd=District.ytd + self.__payment).where((District.id ==
+                                                                  self.__d_id) & (District.w_id == self.__w_id)).execute()
+        Customer.update(
+            balance=Customer.balance - self.__payment,
+            ytd_payment=Customer.ytd_payment + self.__payment,
+            payment_cnt=Customer.payment_cnt + 1
+        ).where((Customer.w_id == self.__w_id) & (Customer.d_id == self.__d_id) & (Customer.id == self.__id)).execute()
+
+        customer = Customer.get(Customer.w_id == self.__w_id,
+                                Customer.d_id == self.__d_id, Customer.id == self.__id)
+        warehouse = Warehouse.get(Warehouse.id == self.__w_id)
+        district = District.get(
+            District.id == self.__d_id, District.w_id == self.__w_id)
+
+        print(f"Customer Info: ")
+        print(f"    Identifier: ({self.__w_id}, {self.__d_id}, {self.__id})")
+        print(f"    Name: {customer.first} {customer.middle} {customer.last}")
+        print(
+            f"    Adress: {customer.street_1}, {customer.street_2}, {customer.city}, {customer.state}, {customer.zip}\n")
+        print(f"Warehouse's Address: ")
+        print(
+            f"    {warehouse.street_1}, {warehouse.street_2}, {warehouse.city}, {warehouse.state}, {warehouse.zip}\n")
+        print(f"District's Address: ")
+        print(
+            f"    {district.street_1}, {district.street_2}, {district.city}, {district.state}, {district.zip}\n")
+        print(f"Payment Amount: ")
+        print(f"    {self.__payment}")
---
+++ transactions/popular_item.py
@@ -0,0 +1,46 @@
+from playhouse.cockroachdb import CockroachDatabase, DatabaseProxy
+from decimal import Decimal
+from datetime import datetime
+
+from models import *
+from transactions.base import BaseTransaction
+
+
+class PopularItemTransaction(BaseTransaction):
+    def __init__(self, warehouse_number, district_number, last_orders_number):
+        self.__warehouse_number = warehouse_number
+        self.__district_number = district_number
+        self.__last_orders_number = last_orders_number
+
+    def _execute_transaction(self):
+
+        print(
+            f"District Identifier: ({self.__warehouse_number}, {self.__district_number})\n")
+        print(f"Number of Last orders: {self.__last_orders_number}\n")
+
+        n = District.get((District.w_id == self.__warehouse_number) &
+                         (District.id == self.__district_number)).next_o_id
+        s = Order.select().where(
+            (Order.d_id == self.__district_number) & (Order.w_id == self.__warehouse_number) & (Order.id.between(n-1-self.__last_orders_number, n-1)))
+
+        for x in s:
+            customer = Customer.get(Customer.id == x.c_id)
+            print(f"\nOrder Info: ")
+            print(f"    Order Number: {x.id}")
+            print(f"    Entry Dazte and Time: {x.entry_d}")
+            print(
+                f"    Customer Name: {customer.first}, {customer.middle}, {customer.last}")
+            ix = OrderLine.select().where(
+                (OrderLine.o_id == x.id) & (OrderLine.d_id == self.__district_number) & (
+                    OrderLine.w_id == self.__warehouse_number)
+            ).order_by(OrderLine.quantity.desc())
+            px = []
+            max_quantity = ix[0].quantity
+            for orderline in ix:
+                if orderline.quantity == max_quantity:
+                    px.append(orderline)
+                else:
+                    break
+            for item in px:
+                print(f"Item Name: {Item.get(Item.id == item.i_id).name}")
+                print(f"Item Quantity: {item.quantity}")
