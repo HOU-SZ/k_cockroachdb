fa8859ab9b3dc9b8c62ffccd2e4e12fcd2e7d512
ZhengYing <zheng@comp.nus.edu.sg>
2021-11-03 17:06:04 +0800

    Refine Stock Level Transaction

--- transactions/stock_level.py
+++ transactions/stock_level.py
@@ -9,22 +9,33 @@ class StockLevelTransaction(BaseTransaction):
         self.__num_order = num_order
     
     def _execute_transaction(self):
-        n = District.get_by_id((self.__w_id, self.__d_id)).next_o_id
-        item_list = set()
-        
-        for i in range(n - self.__num_order, n):
-            order = Order.get_by_id((self.__w_id, self.__d_id, i))
-            for j in range(int(order.ol_cnt)):
-                orderline = Orderline.get_by_id((j+1, self.__w_id, self.__d_id, i))
-                item = Item.get_by_id(orderline.i_id)
-                item_list.add(item.id)
 
-        res = len(list(filter(
-            lambda x: int(Stock.get_by_id((self.__w_id, x)).quantity) < self.__th, 
-            item_list
-        )))
-        
+        orderlines = Orderline.select(
+            Orderline.i_id.alias("i_id")
+        ).join(
+            District,
+            on = (
+                (Orderline.w_id == District.w_id) & 
+                (Orderline.d_id == District.id)
+            )
+        ).distinct().where(
+            (Orderline.w_id == self.__w_id) & 
+            (Orderline.d_id == self.__d_id) & 
+            (Orderline.o_id >= District.next_o_id - self.__num_order)
+        )
+
+        item_num = Stock.select().join(
+            orderlines,
+            on=(
+                (Stock.w_id == self.__w_id) & 
+                (Stock.i_id == orderlines.c.i_id)
+            ),
+        ).where(
+            Stock.quantity < self.__th
+        ).count()
+
+
         print(f"Stock Level: ")
         print(f"    Warehouse: {self.__w_id}")
         print(f"    Order Info: last {self.__num_order} orders in District {self.__d_id}")
-        print(f"    Level: {res}")
+        print(f"    Level: {item_num}")
