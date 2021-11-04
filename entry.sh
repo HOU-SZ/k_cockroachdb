4b3b5e0ad9a721ee88dfa412644ebff247ee2e43
Shizheng Hou [I] <shihou@tesla.com>
2021-11-04 17:40:33 +0800

    Add script for throughput.csv

--- entry.sh
+++ entry.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 task_type=$1
 
 for n in $(seq 30 34)
@@ -7,17 +8,29 @@ do
 {
     server_num=$(($n-30))
     echo "Server cs4224k@xcnc$n starts..."
-    ssh cs4224k@xcnc$n.comp.nus.edu.sg "sh /temp/cs5424_team_k/cockroach-v21.1.7.linux-amd64/CS5424_Project_CockroachDB/runpy.sh $server_num $task_type"
+    sshpass -p "123456" ssh cs4224k@xcnc$n.comp.nus.edu.sg "sh /temp/cs5424_team_k/cockroach-v21.1.7.linux-amd64/CS5424_Project_CockroachDB/runpy.sh $server_num $task_type"
     echo "Server cs4224k@xcnc$n ends..."
  } &
 done
-wait
 
-for n in $(seq 30 34)
-do
-    ssh cs4224k@xcnc$n.comp.nus.edu.sg "cat /temp/cs5424_team_k/cockroach-v21.1.7.linux-amd64/output/task${task_type}.csv" >> result.csv
-done
+# task_type=$1
 
-wait
+# for n in $(seq 30 34)
+# do
+# {
+#     server_num=$(($n-30))
+#     echo "Server cs4224k@xcnc$n starts..."
+#     ssh cs4224k@xcnc$n.comp.nus.edu.sg "sh /temp/cs5424_team_k/cockroach-v21.1.7.linux-amd64/CS5424_Project_CockroachDB/runpy.sh $server_num $task_type"
+#     echo "Server cs4224k@xcnc$n ends..."
+#  } &
+# done
+# wait
 
-echo "All workload $task_type tasks finish..."
+# for n in $(seq 30 34)
+# do
+#     ssh cs4224k@xcnc$n.comp.nus.edu.sg "cat /temp/cs5424_team_k/cockroach-v21.1.7.linux-amd64/output/task${task_type}.csv" >> result.csv
+# done
+
+# wait
+
+# echo "All workload $task_type tasks finish..."
